7b1365a5d57229413157b4482d3d2a90ad2b043a
aehaq <azfarehaq@gmail.com>
2019-02-22 10:29:01 -0800

    index links added

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD w/ PHP</title>
+</head>
+<body>
+    <h1> CRUD demo w/ PHP </h1>
+
+    <div class="nav-section"> 
+        <a class="nav-buttons" href="create.php"> Create </a>
+        <p> Add a database entry </p>
+    </div>
+
+    <div class="nav-section"> 
+        <a class="nav-buttons" href="read.php"> Read </a>
+        <p> View database entries </p>
+    </div>
+
+    <div class="nav-section"> 
+        <a class="nav-buttons" href="update.php"> Update </a>
+        <p> Make changes to a database entry </p>
+    </div>
+
+    <div class="nav-section"> 
+        <a class="nav-buttons" href="delete.php"> Delete </a>
+        <p> Remove a database entry </p>
+    </div>
+    
+</body>
+</html>
