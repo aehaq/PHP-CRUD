64030d5a85df6b2da94393116c6072a17670ad0e
aehaq <azfarehaq@gmail.com>
2019-02-25 15:22:30 -0800

    Create Page functional

--- public/create.php
+++ public/create.php
@@ -1,3 +1,46 @@
+<?php 
+
+require "../config.php";
+
+// The following will run when the form with method 'post' is submitted.
+if (isset($_POST['submit'])) {
+    
+    try {
+
+        // Connecting to the existing database, hence $dsn instead of just host
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        //If the connection was not unsuccessful, the following will be used to post to our database.
+
+        // First we create an array from the POST data
+        $new_user = array(
+            "firstname" => $_POST['firstname'], 
+            "lastname" => $_POST['lastname'], 
+            "location" => $_POST['location'], 
+            "email" => $_POST['email']
+        );
+
+        // the sprintf function allows us to create a formatted string, which will be our dynamically generated sql query
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "users",
+            implode(", ", array_keys($new_user)),
+            '"' .implode('", "', array_values($new_user)).'"'
+        );
+
+        // Prepare SQL Code
+        $statement = $connection->prepare($sql);
+
+        // Execute SQL Code
+        $statement->execute($new_user);
+
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+
+?>
+
 <?php include "templates/header.php"; ?>
 
     <h2> Add contact info </h2>
