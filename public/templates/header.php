88a1b8e5172d87756ce0e272d39aae4d158fd2ff
aehaq <azfarehaq@gmail.com>
2019-03-01 10:54:46 -0800

    header template updated with dynamic link bar

--- public/templates/header.php
+++ public/templates/header.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD w/ PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>PHP CRUD App</title>
 </head>
 <body>
-    <h1> CRUD demo w/ PHP </h1>
+    <nav class="nav">
+        <a <?php if ($page=="create") {echo 'class="nav-link active disabled"';} else {echo 'class="nav-link"';}?> href="../public/create.php">Create</a>
+        <a <?php if ($page=="read") {echo 'class="nav-link active disabled"';} else {echo 'class="nav-link"';}?> href="../public/read.php">Read</a>
+        <a <?php if ($page=="update") {echo 'class="nav-link active disabled"';} else {echo 'class="nav-link"';}?> href="../public/update.php">Update</a>
+        <a <?php if ($page=="delete") {echo 'class="nav-link active disabled"';} else {echo 'class="nav-link"';}?> href="../public/delete.php">Delete</a>
+    </nav>
 
     <?php 
     
@@ -26,6 +32,5 @@
     if ($page == "delete" ) {
         echo "<h1> Delete </h1>";
     }
-
 
     ?>
